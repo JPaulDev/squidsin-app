02c64729e233ec52582656da8279ea656a928918
JonPaulCode <jwatson88@live.co.uk>
2023-05-09 10:54:34 +0100

    Add icon to add expense button and update styles

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,7 +1,10 @@
 import { Account, Activity, Friends, Groups } from '@components/home';
+import { Receipt } from '@icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Pressable, Text } from 'react-native';
+import clsx from 'clsx';
+import { useState } from 'react';
+import { Pressable, Text, View } from 'react-native';
 import type { RootStackParamList } from 'types/RootStackParamsList';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
@@ -9,6 +12,17 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 const Tab = createBottomTabNavigator();
 
 export default function Home({ navigation }: Props): JSX.Element {
+  const [isPressed, setIsPressed] = useState<boolean>(false);
+
+  const handlePress = (): void => {
+    setIsPressed(true);
+    navigation.navigate('Expense');
+
+    setTimeout(() => {
+      setIsPressed(false);
+    }, 200);
+  };
+
   return (
     <>
       <Tab.Navigator
@@ -21,12 +35,23 @@ export default function Home({ navigation }: Props): JSX.Element {
         <Tab.Screen name="Activity" component={Activity} />
         <Tab.Screen name="Account" component={Account} />
       </Tab.Navigator>
-      <Pressable
-        className="absolute bottom-16 right-5 rounded-full bg-emerald-600 px-6 py-3"
-        onPress={() => navigation.navigate('Expense')}
+      <View
+        className={clsx(
+          'absolute bottom-16 right-4 overflow-hidden rounded-full shadow-md shadow-black',
+          isPressed && 'shadow shadow-black'
+        )}
       >
-        <Text className="text-base font-semibold text-white">Add expense</Text>
-      </Pressable>
+        <Pressable
+          className="flex-row items-center bg-emerald-600 px-5 py-3"
+          onPress={handlePress}
+          android_ripple={{ color: '#ffffff33' }}
+        >
+          <Receipt className="fill-white" />
+          <Text className="ml-3 text-base font-semibold text-white">
+            Add expense
+          </Text>
+        </Pressable>
+      </View>
     </>
   );
 }
