cbfbaf760d1daf976d7c87088cbb5eb25b6beb5e
Samuel Clements <samuelclements@Samuels-MacBook-Pro.local>
2023-05-10 15:59:13 +0100

    add current balance to friends

--- components/home/Friends.tsx
+++ components/home/Friends.tsx
@@ -3,16 +3,69 @@ import { Image } from 'expo-image';
 import { Text, View } from 'react-native';
 import { useGetFriendsQuery } from '../../app/services/friends';
 import useAuth from '../../hooks/useAuth';
+import {
+  collection,
+  getDocs,
+  doc,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { db } from '../../firebase';
 
 export default function Friends(): JSX.Element {
   const { user } = useAuth();
   const { data, isLoading } = useGetFriendsQuery(user.uid);
+  const userRef = doc(db, "users", user.uid); 
+  const costsCollectionRef = collection(db, "costs");
+  const [totals, setTotals] = useState({});
+  const [isLoading1,setisLoading1]=useState(true)
+  
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setisLoading1(true);
+      const getTotals = async () => {
+        const newTotals = {};
+        await Promise.all(
+          data.map(async (friend) => {
+            const friendRef = doc(db, "users", friend.uid);
+            const qt = query(
+              costsCollectionRef,
+              where("userPaid", "==", userRef),
+              where("userSplitWith", "==", friendRef),
+              orderBy("timestamp", "desc")
+            );
+            const qts = query(
+              costsCollectionRef,
+              where("userPaid", "==", friendRef),
+              where("userSplitWith", "==", userRef),
+              orderBy("timestamp", "desc")
+            );
+            const querySnapshotOwe = await getDocs(qt); // owing
+            const querySnapshotDue = await getDocs(qts); // due
+            let total = 0;
+            querySnapshotOwe.forEach((docu) => {
+              total += docu.data().cost;
+            });
+            querySnapshotDue.forEach((docu) => {
+              total -= docu.data().cost;
+            });
+            newTotals[friend.uid] = total;
+          })
+        );
+        setTotals(newTotals);
+        setisLoading1(false);
+      };
+      getTotals();
+    }
+  }, [data]);
 
   return (
     <View className="h-full bg-white p-4">
       {isLoading ? (
         <LoadingSpinner />
-      ) : (
+      ) : ( isLoading1 ? (<LoadingSpinner />) :(
         <>
           {data.map((friend) => (
             <View
@@ -24,10 +77,13 @@ export default function Friends(): JSX.Element {
                 className="h-14 w-14 rounded-full"
               />
               <Text className="text-base">{friend.fullName}</Text>
+
+              
+              <Text className="text-base">Balance:  {totals[friend.uid]}</Text>
             </View>
           ))}
         </>
-      )}
+      ))}
     </View>
   );
 }
